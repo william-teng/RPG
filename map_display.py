90ac61c7822ca2ed9a4fe63fbcbda8bf44402845
william-teng <60437273+william-teng@users.noreply.github.com>
2021-05-01 06:09:09 -0600

    Create map_display.py

---
+++ map_display.py
@@ -0,0 +1,30 @@
+# Course: CS 30
+# Period: 1
+# Date: 21/04/30
+# Name: William Teng
+# Description: A visual representation of the map
+
+# The player is on the start tile
+map_display_1 = ["Tile 1 | Tile 2 | Tile 3 "]
+map_display_2 = ["Tile 4 | Tile 5 | Tile 6 "]
+map_display_3 = ["PLAYER | Tile 8 | FINISH "]
+
+# The player is on the dungeon tile
+map_display_4 = ["PLAYER | Tile 2 | Tile 3 "]
+map_display_5 = ["Tile 4 | Tile 5 | Tile 6 "]
+map_display_6 = ["Tile 7 | Tile 8 | FINISH "]
+
+# The player is on the kitchen tile
+map_display_7 = ["Tile 1 | PLAYER | Tile 3 "]
+map_display_8 = ["Tile 4 | Tile 5 | Tile 6 "]
+map_display_9 = ["Tile 7 | Tile 8 | FINISH "]
+
+# The player is on the chapel tile
+map_display_10 = ["Tile 1 | Tile 2 | PLAYER "]
+map_display_11 = ["Tile 4 | Tile 5 | Tile 6 "]
+map_display_12 = ["Tile 7 | Tile 8 | FINISH "]
+
+# The player is on the armoury tile
+map_display_13 = ["Tile 1 | Tile 2 | Tile 3 "]
+map_display_14 = ["Tile 4 | Tile 5 | Tile 6 "]
+map_display_15 = ["Tile 7 | PLAYER | FINISH "]
